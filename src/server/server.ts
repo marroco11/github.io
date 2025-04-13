5020fffffaabdb32faa9878bf4fc483a94eb4647
marroco11 <157555802+marroco11@users.noreply.github.com>
2025-04-12 22:25:56 -0400

    port experiment

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 //Initialize Express app
 
 const app = express();
-const port = process.env.PORT || 3000; // give me the port that the environment gives me or use port 3000
+const port = process.env.PORT || 10000; // give me the port that the environment gives me or use port 3000
 
 async function startServer(){
     try {
